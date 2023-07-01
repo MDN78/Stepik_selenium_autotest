acbc383b5681003370b9a21dafe2dcd09e846007
Дмитрий Максимов <salesakk@gmail.com>
2023-07-01 17:16:37 +0300

    update file test_main_page - new function

--- test_main_page.py
+++ test_main_page.py
@@ -6,5 +6,10 @@ def test_guest_can_go_to_login_page(browser):
     page.open()                     # открываем страницу
     page.go_to_login_page()         # выполняем метод страницы — переходим на страницу логина
 
+def test_guest_should_be_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
 
 # start test: pytest -v --tb=line --browser_name=chrome --language=en test_main_page.py
